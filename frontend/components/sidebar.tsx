02a0ee66460686fbde930db78f2114abbb339df1
UwaoEvan <uwaoevanz@gmail.com>
2024-09-27 11:45:08 +0300

    updated the redux actions to updated the fields

--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -1,12 +1,22 @@
 "use client";
-import { FormContext } from "@/context/form-context";
 import { inputTypeValues } from "@/lib/utils";
+import { useSelector, useDispatch } from "react-redux";
 
-import { useContext } from "react";
+import { RootState } from "@/store";
+import { ADD_FIELD } from "@/store/actions";
 
 export default function SideBar() {
-  const { formFields, handleNewFieldSelect, showPreview } =
-    useContext(FormContext);
+  const { showPreview, formFields } = useSelector((state: RootState) => state.form);
+  const dispatch = useDispatch();
+
+  const addNewField = (id: Number, value: String) => {
+    const newField = {
+      id,
+      question: "",
+      type: value,
+    };
+    dispatch({ type: ADD_FIELD, payload: newField })
+  }
 
   return (
     <>
@@ -15,13 +25,10 @@ export default function SideBar() {
           <div className="flex grow flex-col gap-y-5 overflow-y-auto  px-4 pt-12">
             <div className="ml-4 flex w-80 flex-col rounded-lg border bg-white p-4">
               <h3 className="text-lg font-semibold">Add Question</h3>
-
               {inputTypeValues.map((inputType) => (
                 <button
                   key={inputType.name}
-                  onClick={() =>
-                    handleNewFieldSelect(formFields.length + 1, inputType.name)
-                  }
+                  onClick={() => addNewField(formFields.length + 1, inputType.name)}
                   className="mt-2 rounded bg-blue-500 px-4 py-2 text-left font-bold text-white hover:bg-blue-700"
                 >
                   {inputType.value}
