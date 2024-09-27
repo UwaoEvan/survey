79412334549ea0180b94a285a35ddbf6560afbea
UwaoEvan <uwaoevanz@gmail.com>
2024-09-27 10:07:16 +0300

    implemented redux reducers

---
+++ frontend/store/reducers.ts
@@ -0,0 +1,87 @@
+import {
+  ADD_FIELD,
+  REMOVE_FIELD,
+  UPDATE_FIELD,
+  TOGGLE_PREVIEW,
+  UPDATE_ANSWER_OPTION,
+  UPDATE_TEXT_RESPONSE,
+} from './actions';
+
+const initialState = {
+  fields: [
+    {
+      id: 1,
+      question: "",
+      type: "radio",
+      providedAnswersOptions: [],
+      textAnswer: "",
+    },
+  ],
+  showPreview: false,
+};
+
+const formReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case ADD_FIELD:
+      return {
+        ...state,
+        fields: [
+          ...state.fields,
+          {
+            id: state.fields.length + 1,
+            question: "",
+            type: "radio", 
+            providedAnswersOptions: [],
+            textAnswer: "",
+          },
+        ],
+      };
+
+    case REMOVE_FIELD:
+      return {
+        ...state,
+        fields: state.fields.filter((field) => field.id !== action.payload),
+      };
+
+    case UPDATE_FIELD:
+      return {
+        ...state,
+        fields: state.fields.map((field) =>
+          field.id === action.payload.id
+            ? { ...field, [action.payload.name]: action.payload.value }
+            : field,
+        ),
+      };
+
+    case TOGGLE_PREVIEW:
+      return {
+        ...state,
+        showPreview: !state.showPreview,
+      };
+
+    case UPDATE_ANSWER_OPTION:
+      return {
+        ...state,
+        fields: state.fields.map((field) =>
+          field.id === action.payload.id
+            ? { ...field, providedAnswersOptions: action.payload.options }
+            : field,
+        ),
+      };
+
+    case UPDATE_TEXT_RESPONSE:
+      return {
+        ...state,
+        fields: state.fields.map((field) =>
+          field.id === action.payload.id
+            ? { ...field, textAnswer: action.payload.text }
+            : field,
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default formReducer;
